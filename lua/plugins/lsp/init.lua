ac1fe0d6a4fe29711a4266cf8a261dced1b5e113
Ashan Ghimire <ashanghimire10@gmail.com>
2024-01-18 12:28:25 +0545

    Added phpactor insteadof intelephense

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -26,7 +26,7 @@ M.setup = function()
     lspconfig.rust_analyzer.setup {
         capabilities = capabilities
     }
-    lspconfig.intelephense.setup {
+    lspconfig.phpactor.setup {
         capabilities = capabilities
     }
 
