91ab834ee1613c0c6d7b05ee47ef0faeb729fbe3
Ashan Ghimire <ashanghimire10@gmail.com>
2025-06-12 11:22:14 +0545

    Added transparent background

--- lua/plugins/github-theme.lua
+++ lua/plugins/github-theme.lua
@@ -1,13 +1,17 @@
 local M = {};
 
 M.init = function()
-    return  { 'projekt0n/github-nvim-theme' };
+    return { 'projekt0n/github-nvim-theme' };
 end
 
 M.setup = function()
-    require('github-theme').setup {}
+    require('github-theme').setup({
+        options = {
+            transparent = true,
+        }
+    })
 
-    vim.cmd('colorscheme github_dark_dimmed');
+    vim.cmd('colorscheme github_dark_default');
 end
- 
+
 return M;
